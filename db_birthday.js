ed0cb600422f269a87601ae0b15e32309c6dcf3d
zaa-code <zaaajaaa623@gmail.com>
2026-02-08 10:31:34 +0700

    tambah page ultah

---
+++ db_birthday.js
@@ -0,0 +1,51 @@
+    const dbBirthday = [
+    {
+    "NO": 1,
+    "NO INDUK": 20220206,
+    "NAMA": "Reza Alfiano bin Karsim",
+    "PANGGILAN": "Reza",
+    "TANGGAL": "08\/08\/2009"
+    },
+    {
+    "NO": 2,
+    "NO INDUK": 20220209,
+    "NAMA": "Ghifari Salman Al Rasyid bin Ade Firmansyah",
+    "PANGGILAN": "Ghifari",
+    "TANGGAL": "02\/08\/2009"
+    },
+    {
+    "NO": 3,
+    "NO INDUK": 20220003,
+    "NAMA": "Arima Amanti binti Muhammad Najib",
+    "PANGGILAN": "Arima",
+    "TANGGAL": "08\/02\/2010"
+    },
+    {
+    "NO": 4,
+    "NO INDUK": 20220321,
+    "NAMA": "Naufal Rahman Ibrahim bin Udin Zaenudin",
+    "PANGGILAN": "Naufal",
+    "TANGGAL": "09\/02\/2010"
+    },
+    {
+    "NO": 5,
+    "NO INDUK": 20220312,
+    "NAMA": "Bashar Almukti bin Sairan",
+    "PANGGILAN": "Bashar",
+    "TANGGAL": "10\/02\/2010"
+    },
+    {
+    "NO": 6,
+    "NO INDUK": 20220112,
+    "NAMA": "Muhammad Azriel Ikhlas bin Muhammad Arli Nugraha",
+    "PANGGILAN": "Muhammad",
+    "TANGGAL": "11\/02\/2010"
+    },
+    {
+    "NO": 7,
+    "NO INDUK": 20220322,
+    "NAMA": "Akbar Sholeh Ibrahim bin Ahrom Hidayat",
+    "PANGGILAN": "Akbar",
+    "TANGGAL": "08\/06\/2010"
+    }
+    ];
